ef41d44fccda6801eabcb0d717cc1b6c1f742f3f
halokin <halokin@gmail.com>
2016-06-12 21:11:57 +0200

    form

--- dev/shopping-list-new-item.component.ts
+++ dev/shopping-list-new-item.component.ts
@@ -1,31 +1,54 @@
 import {Component} from 'angular2/core';
 import {ListItem} from "./list-item";
 import {ShoppingListService} from "./shopping-list.service";
+import {ControlGroup} from 'angular2/common';
+import {FormBuilder} from 'angular2/common';
+import {OnInit} from 'angular2/core';
+import {Validators} from 'angular2/common';
+import {Control} from 'angular2/common'
 
 @Component({
 selector: 'shopping-list-new-item',
 template: `
+<form [ngFormModel]="myForm" (ngSubmit)="onSubmit()">
 <div class="input">
 	<label for="item-name">name</label>
-	<input type="text" id="item-name" [(ngModel)]="item.name">
+	<input type="text" id="item-name" [(ngModel)]="item.name" [ngFormControl]="myForm.controls['itemName']">
 </div>
 <div class="input">
 	<label for="item-amt">amount</label>
-	<input type="text" id="item-amt" [(ngModel)]="item.amount">
+	<input type="text" id="item-amt" [(ngModel)]="item.amount" [ngFormControl]="myForm.controls['itemAmount']">
 </div>
-<button (click)="onClick()">add item</button>
+<button type="submit" [disabled]="!myForm.valid">add item</button></form>
+
 `, 
 	
 })
 
 export class ShoppingListNewItemComponent {
 item = {name:'', amount: 0};
+myForm: ControlGroup;
 
-constructor(private _shoppingListService: ShoppingListService) {}
+constructor(private _shoppingListService: ShoppingListService, private _formBuilder: FormBuilder) {}
 
 onClick() {
 	this._shoppingListService.insertItem({name: this.item.name, amount: this.item.amount});
 }
 
+ngOnInit():any {
+	this.myForm = this._formBuilder.group({
+	'itemName': ['', Validators.required],
+	'itemAmount': ['', Validators.compose([
+	Validators.required,
+	greaterZero
+	])],
+	});
+}
 	
+}
+
+function greaterZero(control: Control): {[s: string]: boolean} {
+	if (control.value <= 0){
+	return {notEnough: true};
+	}
 }
